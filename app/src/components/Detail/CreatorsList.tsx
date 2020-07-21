af0c18492cbf75a00b362bfc25289ffd04c9bb3b
Tiago Gomes <t.dfgomes@icloud.com>
2020-07-21 12:53:22 +0100

    clean logs

--- app/src/components/Detail/CreatorsList.tsx
+++ app/src/components/Detail/CreatorsList.tsx
@@ -1,35 +1,32 @@
 import React from 'react'
 import {Creator} from '../../hooks/Comic'
+import { Ul, Li } from '../UI/Lists'
 
 interface CreatorsListProps {
   creators: Creator[]
 }
 
-interface Cr {
-  roles: Creator[]
+interface Acc {
+  [key: string]: Creator[]
 }
 
 
 function CreactorsList({ creators }: CreatorsListProps){
-  console.log('creators ==> ', creators)
-  const creatorsByRole = creators.reduce((acc:any,curr) => {
+
+  const creatorsByRole = creators.reduce((acc: Acc,curr) => {
     acc[curr.role] = creators.filter(creator => creator.role === curr.role)
     return acc
   },{})
-  const roles = Object.keys(creatorsByRole)
-  const cr = Object.values(creatorsByRole)
-  console.log('roles', roles)
-  
-  console.log('creators', cr)
+
   return (
-    <ul>
-      {roles.map(role => (
-        <li key={role}>
+    <Ul>
+      {Object.keys(creatorsByRole).map(role => (
+        <Li key={role}>
           <b>{role}</b>
           <div>{creatorsByRole[role].map(({name}:any) => name).join(', ')}</div>
-        </li>)
+        </Li>)
       )}
-    </ul>
+    </Ul>
   )
 }
 
