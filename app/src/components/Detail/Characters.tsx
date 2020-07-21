f4fd45a862d8d05c7516ca82db8b1a2a13aa2996
Tiago Gomes <t.dfgomes@icloud.com>
2020-07-21 15:57:01 +0100

    add characters to the detail view

---
+++ app/src/components/Detail/Characters.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {useCharacters} from '../../hooks'
+import {Character, UseCharacters} from '../../hooks/Comic'
+import { SectionTitle, P } from '../UI/Typoraphy'
+
+import Img from '../UI/Img'
+import Loader from '../UI/Loader'
+import Grid from '../UI/Grid'
+
+interface CharactersProps {
+  characters: Character[]
+}
+
+function Characters({characters}:CharactersProps){
+  const {data, isLoading}:UseCharacters = useCharacters(characters)
+  
+  return (
+    <>
+      <SectionTitle
+        marginTop={1}
+        marginBottom={0}
+        fontSize={4}
+        lineHeight={6}
+      >
+        Characters:
+      </SectionTitle>
+      {isLoading && <Loader color="primary" />}
+      <Grid variant="container">
+        {data.map(({ thumbnail: {path, extension}, name},idx) => (
+          <Grid variant="item" key={idx}>
+            <P>{name}</P>
+            <Img src={`${path}.${extension}`} alt="character image" height={"thumbnail"}/>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  )
+}
+
+export default Characters
---
+++ app/src/hooks/useCharacters.ts
@@ -0,0 +1,38 @@
+import 'whatwg-fetch'
+
+import { useEffect, useState } from 'react'
+import { API_URL } from '../utils/constants'
+import { Character, CharactersData, UseCharacters, ApiResponse } from './Comic'
+
+export function useCharacters(characters:Character[]): UseCharacters{
+  const [isLoading, setLoading] = useState(true)
+  const [charactersImgs, setCharacter] = useState<CharactersData[]>([])
+  
+  
+  useEffect(() => {
+    const urlsToFetch = characters.map(character => character.resourceURI.split(/public/g).pop())
+    
+    function getCharacters() {
+      setCharacter([]) // clear state each time new characters array is presented 
+
+      Promise.all(urlsToFetch.map( async (url) => {
+        const response = await fetch(`${API_URL}${url}`)
+        const data: ApiResponse<Character> = await response.json()
+
+        setCharacter((prevCharacters) => ([
+          ...prevCharacters,
+          ...data.data.results.map((result: any) => ({thumbnail:result.thumbnail, name:result.name}))
+        ]))
+        
+        
+      })).finally(() => {
+        setLoading(false)
+      })
+    }
+
+    getCharacters()
+    
+  }, [characters])
+  
+  return { isLoading, data: charactersImgs }
+}
