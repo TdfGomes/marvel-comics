309ecad370e0c8dde7c70be6f33ee2a95b01ab68
Tiago Gomes <t.dfgomes@icloud.com>
2020-07-25 00:54:21 +0100

    prettify

--- app/src/test/test-utils.tsx
+++ app/src/test/test-utils.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import {MemoryRouter} from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
 import { render as rtlRender } from '@testing-library/react'
 import ThemeProvider from '../components/UI/ThemeProvider'
 
-function render(ui:any, {  ...options } = {}) {
-const Wrapper = (props: React.PropsWithChildren<{}>) => (
-  <ThemeProvider>
-    <MemoryRouter>
-      {props.children}
-    </MemoryRouter>
-  </ThemeProvider>
-)
+function render(ui: any, { ...options } = {}) {
+  const Wrapper = (props: React.PropsWithChildren<{}>) => (
+    <ThemeProvider>
+      <MemoryRouter>{props.children}</MemoryRouter>
+    </ThemeProvider>
+  )
   return rtlRender(ui, { wrapper: Wrapper, ...options })
 }
 
