538da0b59ab8c5056f919ed8f9f5b14fb13edc2a
Tiago Gomes <t.dfgomes@icloud.com>
2020-07-24 15:40:32 +0100

    change app name

--- app/src/components/App/App.tsx
+++ app/src/components/App/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import { ThemeProvider } from '../UI'
 import Master from '../Master'
 import Detail from '../Detail'
